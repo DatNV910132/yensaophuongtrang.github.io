ac4c5306ff8df107409f54cf5dd5aa30a2de0b39
DatNV910132 <nguyendatbk910@gmail.com>
2022-10-13 02:47:54 -0700

    a
    a

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ if __name__ == '__main__':
     with open("config.json") as f:
         config = json.load(f)
     # runcode
-    app.run(host="0.0.0.0", port=config["port"], debug=True)
+    app.run(host="localhost", port=5000, debug=True)
